d58ff120ecdc1c011c078ca1e4f1906b66bf67bb
Nguyen Minh Tan <minhtan.tm37@gmail.com>
2025-05-13 15:50:33 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ function App() {
             <div className="flex flex-col items-center p-6 bg-[url('/images/pattern-bg-mobile.png')] md:bg-[url('/images/pattern-bg-desktop.png')] bg-cover h-[35%]">
                 <h1 className="text-3xl font-bold text-white">IP Address Tracker</h1>
 
-                <form onSubmit={handleSearch} className="mt-8 flex w-full md:w-1/2">
+                <form onSubmit={handleSearch} className="mt-4 flex w-full md:w-1/2">
                     <input
                         type="text"
                         placeholder="Search for any IP address or domain"
@@ -76,33 +76,33 @@ function App() {
             </div>
 
             <div className="h-[65%] relative">
-                <div className="absolute z-[9999] top-0 inset-x-6 md:mx-auto -translate-y-1/2 bg-white shadow flex flex-col md:flex-row items-center md:items-start p-6 md:px-0 text-center md:text-left justify-between rounded-xl md:w-3/4 space-y-2">
+                <div className="absolute z-[9999] top-0 inset-x-6 md:mx-auto -translate-y-1/2 bg-white shadow flex flex-col md:flex-row items-center md:items-start p-4 md:py-8 md:px-0 text-center md:text-left justify-between rounded-xl md:w-3/4 space-y-1">
                     <div className="flex-1">
                         <p className="font-bold md:px-8">
                             <span className="uppercase text-[#969696] text-xs">IP Address</span>
                             <br />
-                            <span className="text-xl">{location?.ip}</span>
+                            <span className="text-lg md:text-xl">{location?.ip}</span>
                         </p>
                     </div>
                     <div className="flex-1">
                         <p className="font-bold md:border-l-2 md:px-8">
                             <span className="uppercase text-[#969696] text-xs">Location</span>
                             <br />
-                            <span className="text-xl">{location ? `${location.city}, ${location.country}` : ''}</span>
+                            <span className="text-lg md:text-xl">{location ? `${location.city}, ${location.country}` : ''}</span>
                         </p>
                     </div>
                     <div className="flex-1">
                         <p className="font-bold md:border-l-2 md:px-8">
                             <span className="uppercase text-[#969696] text-xs">Timezone</span>
                             <br />
-                            <span className="text-xl">{location && `UTC ${location.timezone_gmt}`}</span>
+                            <span className="text-lg md:text-xl">{location && `UTC ${location.timezone_gmt}`}</span>
                         </p>
                     </div>
                     <div className="flex-1">
                         <p className="font-bold md:border-l-2 md:px-8">
                             <span className="uppercase text-[#969696] text-xs">ISP</span>
                             <br />
-                            <span className="text-xl">{location?.isp}</span>
+                            <span className="text-lg md:text-xl">{location?.isp}</span>
                         </p>
                     </div>
                 </div>
